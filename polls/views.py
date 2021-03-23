52219febaea1c3d20e61542299fbb334910d723d
RayDia <dyl594560@outlook.com>
2021-03-23 20:11:09 +0800

    add polls-detail,results,vote

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello Ray ,this is first app - polls")
+
+def detail(request, question_id):
+    return HttpResponse("Hello.\n  This is Question %s." % question_id)
+
+def results(request, question_id):
+    response = "Hello.\n  This is the result of Question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting for Question %s." % question_id)
